5dbf1115e8961e24ec77da7d11cd4abdc5e4131d
Daniel Bastidas <danielbastidasramirez@gmail.com>
2018-02-15 15:14:28 +0000

    Initial Commit

---
+++ iOS-ReviewTests/iOS_ReviewTests.swift
@@ -0,0 +1,36 @@
+//
+//  iOS_ReviewTests.swift
+//  iOS-ReviewTests
+//
+//  Created by Daniel Bastidas Ramirez on 15/02/2018.
+//  Copyright © 2018 Daniel Bastidas. All rights reserved.
+//
+
+import XCTest
+@testable import iOS_Review
+
+class iOS_ReviewTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
