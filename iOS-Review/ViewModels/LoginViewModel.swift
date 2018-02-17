d25898bc06c83ad2786b66829a84b429adc4842d
Daniel Bastidas <danielbastidasramirez@gmail.com>
2018-02-17 12:00:00 +0000

    Added Basic validation using RxSwift and RxCocoa to manage UI interations

---
+++ iOS-Review/ViewModels/LoginViewModel.swift
@@ -0,0 +1,39 @@
+//
+//  LoginViewModel.swift
+//  iOS-Review
+//
+//  Created by Daniel Bastidas Ramirez on 16/02/2018.
+//  Copyright © 2018 Daniel Bastidas. All rights reserved.
+//
+
+import Foundation
+import RxSwift
+
+class  LoginViewModel{
+   
+    private var isValid :Observable<Bool>
+    private var disposeBag : DisposeBag = DisposeBag()
+    
+    
+    private var loginView: LoginView
+    
+
+    init(loginView: LoginView) {
+
+        self.loginView = loginView
+
+        self.isValid = Observable.combineLatest(loginView.rxEmailText,loginView.rxPasswordText){
+            email,password in
+            return email.count >= 5 && password.count >= 5
+        }
+
+        isValid.subscribe(onNext: {
+            (isValid) in
+            loginView.bindLabelActive(enabled: isValid)
+            loginView.bindToButtonActive(enabled: isValid)
+        }).disposed(by: disposeBag)
+        
+    }
+    
+   
+}
---
+++ iOS-Review/Views/LoginView.swift
@@ -0,0 +1,51 @@
+//
+//  ViewController.swift
+//  iOS-Review
+//
+//  Created by Daniel Bastidas Ramirez on 15/02/2018.
+//  Copyright © 2018 Daniel Bastidas. All rights reserved.
+//
+
+import UIKit
+import RxSwift
+import RxCocoa
+
+class LoginView: UIViewController {
+    
+
+    //UI OUTLETS
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var enabledLabel: UILabel!
+    @IBOutlet weak var loginButton: UIButton!
+    
+    // Rx vars
+    var rxEmailText: Observable<String>!
+    var rxPasswordText: Observable<String>!
+
+    //Dependency Injections
+    var loginViewModel:LoginViewModel!
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //Bind Observables from the UI
+        self.rxEmailText = emailTextField.rx.text.map{$0 ?? ""}
+        self.rxPasswordText = passwordTextField.rx.text.map{$0 ?? ""}
+        
+        self.loginViewModel = LoginViewModel(loginView: self)
+
+    }
+    
+    func bindToButtonActive(enabled:Bool){
+        loginButton.isEnabled = enabled
+    }
+    
+    func bindLabelActive(enabled:Bool){
+        enabledLabel.text = enabled ? "Enabled" : "Not Enabled"
+        enabledLabel.textColor = enabled ? .green : .red
+    }
+
+}
+
